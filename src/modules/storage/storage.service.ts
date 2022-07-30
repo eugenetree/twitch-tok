d2215e3052ccef65374abd3080e6080c8c15aa08
Eugene Derevyanko <eugenedrvnk@gmail.com>
2022-07-30 16:57:07 +0300

    update storage service, updae typeorm version

--- src/modules/storage/storage.service.ts
+++ src/modules/storage/storage.service.ts
@@ -3,19 +3,11 @@ import path from 'path';
 import fs from 'fs';
 import { StorageService } from './storage.type';
 
+const VIDEO_PATH = '/storage/videos/';
+
 @Injectable()
 export class DefaultStorageService implements StorageService {
-  private readonly storagePath: string = path.resolve('storage');
-  private readonly videosPath: string = path.resolve(this.storagePath, 'videos');
-
-  constructor() {
-    fs.mkdirSync(this.storagePath, { recursive: true });
-    fs.mkdirSync(this.videosPath, { recursive: true });
-  }
-
-  getFolderPathForVideo(entityId: number): string {
-    const dirPath = path.resolve(this.videosPath, String(entityId));
-    fs.mkdirSync(dirPath, { recursive: true });
-    return dirPath;
+  getFolderPathForVideo: StorageService['getFolderPathForVideo'] = (entityId) => {
+    return `${VIDEO_PATH}/${entityId}.mp4`;
   }
 }
